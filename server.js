22c0ae6a3529000ce730017e930ca2e8971f723d
Patrick Kennedy <pkennedytx1@gmail.com>
2021-10-04 20:14:32 -0500

    showed a few ways of making use of the node api

--- server.js
+++ server.js
@@ -1,12 +1,37 @@
-const express = require("express");
+import express from 'express';
+import bodyParser from 'body-parser';
+import { Low, JSONFile } from 'lowdb';
+import { fileURLToPath } from 'url'
+
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+const adapter = new JSONFile('db.json');
+const db = new Low(adapter);
+await db.read();
+
+import path from 'path';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // Example Get Request
-app.get("/greetings", (req, res, next) => {
-    res.json(["Hello","Sup","Howdy","Greetings"]);
+app.get("/coffees", (req, res, next) => {
+    const coffees = db.data.coffees.map((coffee) => {
+        return coffee;
+    })
+    res.json(coffees);
 });
 
+app.get("/", (req, res) => {
+    res.sendFile('index.html', { root: __dirname });
+})
+
 // ToDo: Make a post request to our db
+app.post("/newcoffee", (req, res) => {
+    const newCoffee = req.body;
+    db.data.coffees.push(newCoffee);
+    db.write();
+    res.sendStatus(200);
+})
 
 // TODO: Make a new get request from the db
 
